dc10db48d92e8270887f651f5a264f58a54610b7
Berik Mukin <55436981+snoudens@users.noreply.github.com>
2022-03-31 19:53:44 +0600

    Delete check.php

--- check.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-$subject = 'the subject';
-$headers = 'From: $emailr\nReply-to: $emailr\nContent-type:text/html;charset=utf-8\r\n';
-
-mail("blackstyle174@gmail.com", $subject, $message, $header);
- ?>
